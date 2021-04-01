46ea8bd4c2357b203ae3e0c2a2ab1c15c30e57ee
William Brebion <36194526+Okcase@users.noreply.github.com>
2021-04-01 10:08:40 +0200

    Add files via upload

--- ParMaxAut.py
+++ ParMaxAut.py
@@ -1,21 +1,25 @@
+import copy
+
 
 class Task:
-    name = ""  # Nom de la tâche, unique.
-    reads = []  # Domaine de lecture de la tâche
-    writes = []  # Domaine d'écriture de la tâche
-    run = None  # Fonction pour le comportement de la tâche
+    def __init__(self, name, reads, writes, run):
+        self.name = name  # Nom de la tâche, unique.
+        self.reads = reads  # Domaine de lecture de la tâche
+        self.writes = writes  # Domaine d'écriture de la tâche
+        self.run = run  # Fonction pour le comportement de la tâche
 
 
 class TaskSystem:
-    taskList = list()  # Liste des tâches
-    dictionary = dict()  # Dictionnaire des tâches avec leurs dépendances
+    def __init__(self, taskList, dictionary):
+        self.taskList = taskList  # Liste des tâches
+        self.dictionary = dictionary  # Dictionnaire des tâches avec leurs dépendances
 
-    def fillDict(self, taskList, dictionary):
+    def fillDict(self, taskList, dictionary):  # Remplit le dictionnaire avec la liste des tâches
         for i in range(len(taskList)):
             dictionary[taskList[i].name] = list()
 
-    def getDependencies(self, task, dictionary):  # Retourne la liste des dépendances d'une tâche
-        return dictionary[task.name]
+    def getDependencies(self, task):  # Retourne la liste des dépendances d'une tâche
+        return self.dictionary[task.name]
 
     def bernstein(self, task1, task2, dictionary):  # Evaluation des conditions de Bernstein
         for i in task1.writes:
@@ -29,16 +33,18 @@ class TaskSystem:
         for i in task1.writes:
             for j in task2.writes:
                 if i == j:
-                    dictionary[task1.name].append(task2.name)
-                    dictionary[task2.name].append(task1.name)
+                    pass
+                    # dictionary[task1.name].append(task2.name)
+                    # dictionary[task2.name].append(task1.name)
 
-    def dependencies(self, taskList, dictionary):  # Cherche toutes les dépendances entre les tâches
-        for i in range(0, len(taskList) - 1):
-            for j in range(1, len(taskList)):
-                self.bernstein(taskList[i], taskList[j], dictionary)
+    def dependencies(self, dictionary):  # Cherche toutes les dépendances entre les tâches
+        for i in range(0, len(self.taskList) - 1):
+            for j in range(1, len(self.taskList)):
+                self.bernstein(self.taskList[i], self.taskList[j], dictionary)
 
     def run(self):
-        print()
+        self.dependencies(self.dictionary)
+        dico = copy.deepcopy(self.dictionary)
 
 
 X = None
@@ -61,21 +67,21 @@ def runTsomme():
     Z = X + Y
 
 
-t1 = Task()
-t1.name = "T1"
+t1 = Task("T1", [""], ["X"], runT1)
+"""t1.name = "T1"
 t1.writes = ["X"]
-t1.run = runT1
+t1.run = runT1"""
 
-t2 = Task()
-t2.name = "T2"
+t2 = Task("T2", [""], ["Y"], runT2)
+"""t2.name = "T2"
 t2.writes = ["Y"]
-t2.run = runT2
+t2.run = runT2"""
 
-tSomme = Task()
-tSomme.name = "somme"
+tSomme = Task("somme", ["X", "Y"], ["Z"], runTsomme)
+"""tSomme.name = "somme"
 tSomme.reads = ["X", "Y"]
 tSomme.writes = ["Z"]
-tSomme.run = runTsomme
+tSomme.run = runTsomme"""
 
 """t1.run()
 t2.run()
@@ -84,8 +90,6 @@ print(X)
 print(Y)
 print(Z)"""
 
-s1 = TaskSystem()
-s1.taskList = [t1, t2, tSomme]
-s1.fillDict(s1.taskList, s1.dictionary)
-s1.dependencies(s1.taskList, s1.dictionary)
+s1 = TaskSystem([t1, t2, tSomme], {"T1": [], "T2": ["T1"], "somme": ["T1", "T2"]})
+# s1.dependencies(s1.taskList, s1.dictionary)
 print(s1.dictionary)
